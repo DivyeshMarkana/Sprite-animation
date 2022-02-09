d067d16b2598ca6212970a489af6c94aba7eb4e7
Divyesh Markana <divyeshmarkana724@gmail.com>
2022-02-09 12:36:22 +0530

    set interval to animate image

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+let tID; // use varible for clear setInterval()
+
 function animateScript() {
-    document.getElementById("image").style.backgroundPosition = `-230px 0px`;
+
+    let position = 213.33; // start position
+    const interval = 90 // for setInterval()
+
+    tID = setInterval( () => {
+        document.getElementById("image").style.backgroundPosition = `${position}px 0px`;
+
+        if (position < 1280) {
+            position = position + 213.33;
+        } else {
+            position = 213.33;
+        }
+    }, interval)
 }
