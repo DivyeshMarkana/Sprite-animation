08e628f5399486465e77cecefab37a9022f07203
Divyesh Markana <divyeshmarkana724@gmail.com>
2022-02-09 12:41:11 +0530

    stop animate on mouse out

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ function animateScript() {
 
     let position = 213.33; // start position
     const interval = 90 // for setInterval()
+    const diff = 213.33;
 
     tID = setInterval( () => {
         document.getElementById("image").style.backgroundPosition = `${position}px 0px`;
 
         if (position < 1280) {
-            position = position + 213.33;
+            position = position + diff;
         } else {
             position = 213.33;
         }
     }, interval)
+}
+
+function stopAnimate() {
+    clearInterval(tID)
 }
